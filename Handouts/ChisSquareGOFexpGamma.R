d68ffecf0430779968f688b74f26645d760ca6d4
Arnholt <arnholtat@ip-152010200110.math.appstate.edu>
2014-03-05 11:42:58 -0500

    add some code

--- Handouts/ChisSquareGOFexpGamma.R
+++ Handouts/ChisSquareGOFexpGamma.R
@@ -18,6 +18,20 @@ pvalue <- pchisq(X2obs, 10 - 1 - 2, lower = FALSE)
 pvalue
 curve(dchisq(x, 7), 0, 25)
 abline(v = X2obs, col = "red", lty = "dashed")
+#########
+qs <- qgamma(seq(0, 1, by = 0.1), 3, 6)
+OB <- cut(stuff, breaks = qs, include.lowest = TRUE)
+T1 <- xtabs(~OB)
+OBS <- as.vector(T1)
+OBS
+EXP <- rep(10, 10)
+X2obs <- sum((OBS - EXP)^2/EXP)
+X2obs
+pvalue <- pchisq(X2obs, 10 - 1, lower = FALSE)
+pvalue
+curve(dchisq(x, 9), 0, 25)
+abline(v = X2obs, col = "red", lty = "dashed")
+
 ####################################################
 hist(stuff, freq= FALSE, col = "peru")
 d.stuff <- density(stuff)
