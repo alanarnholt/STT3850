6488b7d2f48f31f6428f6e61ca9aaf04112d43b2
Alan T. Arnholt <arnholtat@appstate.edu>
2022-03-29 10:44:54 -0400

    tweaks

--- Data/student-merge.R
+++ Data/student-merge.R
@@ -11,3 +11,39 @@ library(janitor)
 d3 <- d3 %>% 
   clean_names()
 glimpse(d3)
+# g3_x - mathematics
+# g3_y - portugeses
+
+d3 %>% 
+  group_by(sex) %>% 
+  summarize(Mean = mean(g3_x), SD = sd(g3_x), n = n())
+#
+t.test(d3$g3_x ~ d3$sex)
+#
+fm_grades <- d3 %>% 
+  filter(sex == "F") %>% 
+  select(g3_x) %>% 
+  pull()
+mm_grades <- d3 %>% 
+  filter(sex == "M") %>% 
+  select(g3_x) %>% 
+  pull()
+summary(fm_grades)
+summary(mm_grades)
+
+#####
+# Bootstrap T
+
+B <- 10000
+BST <- numeric(B)
+for(i in 1:B){
+  bss1 <- sample(fm_grades, 198, replace = TRUE)
+  bss2 <- sample(mm_grades, 184, replace = TRUE)
+  BST[i] <- ((mean(bss1) - mean(bss2)) - (mean(fm_grades) - mean(mm_grades))) / sqrt(var(bss1)/198 + var(bss2)/184)
+}
+Q <- quantile(BST, probs = c(0.025, 0.975))
+Q
+
+CI <- c( (mean(fm_grades) - mean(mm_grades)) -Q[2]*sqrt(var(fm_grades)/198 + var(mm_grades)/184),
+         (mean(fm_grades) - mean(mm_grades)) -Q[1]*sqrt(var(fm_grades)/198 + var(mm_grades)/184) )
+CI
