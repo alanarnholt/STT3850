b02faa49e8cbb950dc9d85c46911becb494e2af4
Alan T. Arnholt <arnholtat@appstate.edu>
2022-03-29 10:32:03 -0400

    tweaks

---
+++ Data/student-merge.R
@@ -0,0 +1,13 @@
+d1=read.table("student-mat.csv",sep=";",header=TRUE)
+d2=read.table("student-por.csv",sep=";",header=TRUE)
+
+d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+print(nrow(d3)) # 382 students
+
+
+# Alan's additions
+library(tidyverse)
+library(janitor)
+d3 <- d3 %>% 
+  clean_names()
+glimpse(d3)
