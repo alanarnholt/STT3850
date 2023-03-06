c4621cb1cac0004866e5923835d635294573c44e
Alan T. Arnholt <arnholtat@appstate.edu>
2023-03-06 09:50:56 -0500

    tweakers

--- Rscripts/MiscBinom.R
+++ Rscripts/MiscBinom.R
@@ -8,9 +8,13 @@ hist(xs)
 mean(xs <= 12 & xs >=5)
 
 z <- 0:300
-pz <- dbinom(x, 300, .03)
+pz <- dbinom(z, 300, .03)
 
 EVX <- function(x, px){
+  if(sum(px) != 1)
+    stop("sum of probabilities must be 1.")
+  if(length(x) != length(px))
+    stop("x and px must be the same length.")
   E <- sum(x*px)
   V <- sum(((x - E)^2)*px)
   S <- sqrt(V)
@@ -19,13 +23,54 @@ EVX <- function(x, px){
 
 EVX(z, pz)
 
+# Expected Values #2
+x <- 1:3
+px <- c(.53, .47*.53, .47^2*.53 + .47^3)
+EVX(x, px)
+# E(Boys)
+x <- 0:3
+px <- c(.53, .47*.53, .47^2*.53, .47^3)
+EVX(x, px)
+# SD # 1
+x <- c(0, 13, 26, 41)
+px <- c(26/52, 13/52, 12/52, 1/52)
+EVX(x, px)
+# SD # 2
+x <- 1:3
+px <- c(.46, .54*.46, 1 - .46 - .54*.46)
+EVX(x, px)
+# E value and variance # 1
+x <- c(110 - 9000, 110 - 2000, 110)
+px <- c(1/2083, 1/495, 1 -(1/2083 + 1/495))
+EVX(x, px)
+
+--------------------------------------------
+
+z <- 0:300
+pz <- dbinom(z, 300, .03)
+EVX(z, pz)
+# E(Z) = 9.0, S(X) = 2.954657
+# Y ~approx N(9, 2.954657)
+# Solve Exactly
+# P(6 <= Z <= 12) = P(Z <= 12) - P(Z <= 5)
+pbinom(12, 300, 0.03) - pbinom(5, 300, 0.03)
+# Or
+sum(dbinom(6:12, 300, 0.03))
+# Approximation now
+# P(6 <= Y <= 12)
+pnorm(12, 9, 2.954657) - pnorm(6, 9, 2.954657)  
+  
+################### 
+
+# Suppose IQ ~ N(100, 16)
+
+# P(84 <= IQ <= 116)  = P(IQ <= 116) - P(IQ <= 84) 
 pnorm(116, 100, 16) - pnorm(84, 100, 16)
 
+# P(IQ <= x) = 0.95
+qnorm(.95, 100, 16)
 
-
-EX(z, pz)
-
-
+#
 
 
 
