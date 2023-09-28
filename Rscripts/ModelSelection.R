3a493c3e01f0d11bd60fc1c7ec74dd708508c335
Alan T. Arnholt <arnholtat@appstate.edu>
2023-09-28 09:17:28 -0400

    tweakers

--- Rscripts/ModelSelection.R
+++ Rscripts/ModelSelection.R
@@ -19,6 +19,7 @@ modBACK
 modFORW <- stepAIC(null, scope = list(lower = null, upper = full), direction = "forward", test = "F")
 modFORW
 modBACK
+##########################################################
 
 modpl <- lm(Sales ~ Price + ShelveLoc, data = Carseats)
 summary(modpl)
@@ -27,10 +28,59 @@ library(tidyverse)
 ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
   geom_point() + 
   geom_parallel_slopes(se = FALSE)
-
+##### Parallel Lines by hand....
 ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
   geom_point() + 
-  geom_smooth(method= "lm", se = FALSE)
-
-modds <- lm(Sales ~ Price + ShelveLoc + Price:ShelveLoc, data = Carseats)
-summary(modds)
+  geom_abline(slope = -0.056698, intercept = 12.001802, color = "red") + 
+  geom_abline(slope = -0.056698, intercept = 12.001802 + 1.862022, color = "blue") +
+  geom_abline(slope = -0.056698, intercept = 12.001802 + 4.895848, color = "darkgreen") +
+  xlim(0, 200) +
+  theme_bw()
+####### 
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1], color = "red") + 
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1] + coef(modpl)[4], color = "blue") +
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1] + coef(modpl)[3], color = "darkgreen") +
+  xlim(0, 200) +
+  theme_bw()
+#########
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1], color = "red") + 
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1] + coef(modpl)[4], color = "blue") +
+  geom_abline(slope = coef(modpl)[2], intercept = coef(modpl)[1] + coef(modpl)[3], color = "darkgreen") +
+  xlim(0, 200) +
+  theme_bw() + 
+  geom_parallel_slopes()
+###########
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_smooth(method= "lm", se = FALSE) + 
+  theme_bw()
+# What model is creating the above graph? The interaction model.
+modint <- lm(Sales ~ Price + ShelveLoc + Price:ShelveLoc, data = Carseats)
+summary(modint)
+## Graphing the model
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_abline(slope = -0.055220, intercept = 11.832984, color = "red") + 
+  geom_abline(slope = -0.055220 + 0.001984, intercept = 11.832984 + 1.630481, color = "blue") +
+  geom_abline(slope = -0.055220 - 0.010564, intercept = 11.832984 + 6.135880, color = "darkgreen") +
+  theme_bw()
+#################
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_abline(slope = -0.055220, intercept = 11.832984, color = "red") + 
+  geom_abline(slope = -0.055220 + 0.001984, intercept = 11.832984 + 1.630481, color = "blue") +
+  geom_abline(slope = -0.055220 - 0.010564, intercept = 11.832984 + 6.135880, color = "darkgreen") +
+  theme_bw() + 
+  geom_smooth(method = "lm")
+################
+ggplot(data = Carseats, aes(x = Price, y = Sales, color = ShelveLoc)) + 
+  geom_point() + 
+  geom_abline(slope = coef(modint)[2], intercept = 11.832984, color = "red") + 
+  geom_abline(slope = coef(modint)[2] + coef(modint)[4], intercept = coef(modint)[1] + coef(modint)[4], color = "blue") +
+  geom_abline(slope = coef(modint)[2] + coef(modint)[5], intercept = coef(modint)[1] + coef(modint)[3], color = "darkgreen") +
+  theme_bw() + 
+  geom_smooth(method = "lm")
