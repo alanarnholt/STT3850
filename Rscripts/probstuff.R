52f1004965e058d7d772580ada7b6c433080fdbf
Alan T. Arnholt <arnholtat@appstate.edu>
2024-02-28 09:58:13 -0500

    tweaker

--- Rscripts/probstuff.R
+++ Rscripts/probstuff.R
@@ -1,1 +1,12 @@
 # Notes 2/28/2024
+S <- expand.grid(die1 = 1:6, die2 = 1:6)
+S$Sum <- apply(S, 1, sum)
+S
+library(MASS)
+fractions(sum(S$die1 == 3 & S$Sum == 6)/sum(S$die1 ==3))
+
+S <- expand.grid(die1 = 1:20, die2 = 1:20, die3 = 1:20)
+S$Sum <- apply(S, 1, sum)
+S
+sum(S$die1 == 12)
+fractions(sum(S$die1 ==12 & S$Sum == 46)/sum(S$die1 == 12))
