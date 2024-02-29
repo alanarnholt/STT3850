2897c9b648118fe7a806bcdd0abd4a4092677034
Alan T. Arnholt <arnholtat@appstate.edu>
2024-02-29 09:10:13 -0500

    tweakers

--- Rscripts/probstuff.R
+++ Rscripts/probstuff.R
@@ -1,14 +1,8 @@
 # Notes 2/28/2024
+
 S <- expand.grid(die1 = 1:6, die2 = 1:6)
 S$Sum <- apply(S, 1, sum)
 S
-
-
-
-
-
-
-
 
 S$die1 == 3
 S$Sum == 6
@@ -17,35 +11,13 @@ sum(S$die1 == 3)
 sum(S$Sum == 6)
 sum(S$die1 == 3 & S$Sum == 6)
 
-
-
 library(MASS)
 # P(sum =6 | die1 = 3)
 fractions(sum(S$die1 == 3 & S$Sum == 6)/sum(S$die1 ==3))
 
 
-
-
-
-
 # P(sum >= 6|die1 = 3)
 fractions(sum(S$die1 == 3 & S$Sum >= 6)/sum(S$die1 ==3))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 S <- expand.grid(die1 = 1:20, die2 = 1:20, die3 = 1:20)
 S$Sum <- apply(S, 1, sum)
@@ -54,3 +26,15 @@ sum(S$die1 == 10)
 sum(S$Sum >= 30)
 sum(S$die1 == 10 & S$Sum >= 30)
 fractions(sum(S$die1 == 10 & S$Sum >= 30)/sum(S$die1 == 10))
+
+table(S$Sum)
+T1 <- table(S$Sum)/20^3
+T1
+X <- 3:60
+EX <- sum(X*T1)
+EX
+sum(T1)
+VX <- sum((X - EX)^2*T1)
+VX
+SX <- sqrt(VX)
+SX
