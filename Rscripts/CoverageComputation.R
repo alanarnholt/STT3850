915be0b663753419f23151105cdfb9be236866e1
Alan T. Arnholt <arnholtat@appstate.edu>
2024-04-03 11:50:37 -0400

    twekas

--- Rscripts/CoverageComputation.R
+++ Rscripts/CoverageComputation.R
@@ -6,7 +6,10 @@ m.err <-qnorm(1-alpha/2)*sqrt(sp*(1-sp)/n)
 lcl <-sp-m.err  
 ucl <-sp + m.err 
 pp <- 0.79 #pp =P(Success) 
-prob <-dbinom(x,n,pp) 
+prob <-dbinom(x, n, pp) 
 cover <-(pp >= lcl) & (pp <= ucl) #vector of 0s and 1s 
 RES <-round(cbind(x, sp, m.err, lcl, ucl, prob, cover), 4)
 sum(dbinom(x[cover], n,pp)) #total coverage prob at pp
+
+x[cover]
+dbinom(x[cover], n,pp)
