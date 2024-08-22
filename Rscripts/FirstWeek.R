137bd8bfd2ef6eb5dc3891b6063e6c9d25df0964
Alan T. Arnholt <arnholtat@appstate.edu>
2024-08-22 11:39:35 -0400

    tweaks

--- Rscripts/FirstWeek.R
+++ Rscripts/FirstWeek.R
@@ -2,21 +2,17 @@
 library(tidyverse)
 library(palmerpenguins)
 penguins
-ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm, color = species)) + 
+str(penguins) 
+
+mean(penguins$bill_length_mm, na.rm = TRUE)
+
+penguins |> 
+  filter(species == "Adelie") -> penAD
+penAD
+
+ggplot(data = penguins, aes(x = bill_length_mm, 
+                            y = bill_depth_mm,
+                            color = species)) +
   geom_point() + 
-  geom_smooth(method = "lm") 
+  geom_smooth(method = "lm", se = FALSE)
 
-## Suppose we want to work with only Chinstrap
-
-penChin <- penguins |> 
-  filter(species == "Chinstrap")
-penChin
-
-ggplot(data = penChin, aes(x = bill_length_mm, y = bill_depth_mm)) + 
-  geom_point(color = "red") + 
-  geom_smooth(method = "lm", color = "purple") +
-  theme_classic() + 
-  labs(x = "Bill length in mm", y = "Bill depth in mm",
-       title = "You put something here",
-       subtitle = "Where did the data come from?",
-       caption = "Data: penguins")
