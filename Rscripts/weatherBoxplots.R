e5e997c90a8feb5a007a4d4c83d62cebc8c91b8b
Alan T. Arnholt <arnholtat@appstate.edu>
2024-08-29 10:36:30 -0400

    weather stuff

--- Rscripts/weatherBoxplots.R
+++ Rscripts/weatherBoxplots.R
@@ -16,9 +16,12 @@ ggplot(data = weather, aes(x = as.factor(month), y = temp)) +
 # Some better
 
 weather |> mutate(monthT = factor(month, labels = month.abb)) -> weather
-
-ggplot(data = weather, aes(x = monthT, y = temp)) + 
-  geom_boxplot()
+head(weather$monthT)
+ggplot(data = weather, aes(x = monthT, y = temp, fill = monthT)) + 
+  geom_boxplot() + coord_flip() + theme_classic() + 
+  labs(x = "", 
+       y = "Temperature in Fahrenheit",
+       title = "Temperature at NYC airports") + guides(fill = "none")
 
 # 
 
