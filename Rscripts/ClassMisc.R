0fb54ef93f54b1b58c89c416704ed33ec1d45082
Alan T. Arnholt <arnholtat@appstate.edu>
2024-11-14 09:15:24 -0500

    tweaks

--- Rscripts/ClassMisc.R
+++ Rscripts/ClassMisc.R
@@ -38,4 +38,30 @@ index <- sample(10, 5, replace = FALSE)
 
 
 
+library(tidyverse) 
+library(infer) 
+library(moderndive) 
+library(nycflights13) 
+library(ggplot2movies)
 
+promotions
+tapply(promotions$decision == "promoted", promotions$gender, mean)
+(obs_diff <- -diff(tapply(promotions$decision == "promoted", promotions$gender, mean)))
+
+B <- 10^5-1
+pd <- numeric(B)
+for(i in 1:B){
+  pd[i] <- -diff(tapply(promotions$decision == "promoted", sample(promotions$gender), mean))
+}
+hist(pd)
+(pv <- (sum(pd >= obs_diff) + 1)/(B + 1))
+
+#####
+promotions |> 
+  specify(decision ~ gender, success = "promoted") |> 
+  hypothesize(null = "independence") |> 
+  generate(reps = 10^5 - 1, type = "permute") |> 
+  calculate(stat = "diff in props", order = c("male", "female")) -> pd
+pd
+get_pvalue(pd, obs_stat = obs_diff, direction = "right")
+((sum(pd$stat >= obs_diff) + 1)/(B + 1) -> pv2)
