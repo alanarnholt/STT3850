9a8c520b29fcb6fe4146193923ba88c6ef400c36
Alan T. Arnholt <arnholtat@appstate.edu>
2025-02-20 08:45:50 -0500

    tweakers

--- Rscripts/cars2018.R
+++ Rscripts/cars2018.R
@@ -70,13 +70,19 @@ cars_sub_premium_auto <- cars2018 |>
   filter(transmission == "Automatic", 
          recommended_fuel == "Premium Unleaded Recommended")
 
-
+# Recall
+full_mod
+# Graph now
 ggplot(data = cars_sub_premium_auto, 
        aes(x = displacement, y = log10_mpg, color = drive)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
-  theme_bw() 
-# Or
+  theme_bw() +
+  geom_abline(intercept = 1.50940, slope = -0.04225, color = "red", lty = "dashed") + 
+  geom_abline(intercept = 1.50940 + 0.03161, slope = -0.04225 -0.01603, color = "green", lty = "dashed") +
+  geom_abline(intercept = 1.50940 + 0.00283, slope = -0.04225 -0.01431, color = "purple", lty = "dashed") +
+  geom_abline(intercept = 1.50940 + 0.02494, slope = -0.04225 -0.02172, color = "lightblue", lty = "dashed") 
+# Which plot do we use?
 ggplot(data = cars_sub_premium_auto, 
        aes(x = displacement, y = log10_mpg, color = drive)) + 
   geom_point() + 
@@ -116,3 +122,4 @@ ggplot(data = cars_sub_premium_auto,
   geom_abline(intercept = RW2, slope = SLOPE, color = "green", lty = "dashed") + 
   geom_abline(intercept = FourW, slope = SLOPE, color = "lightblue", lty = "dashed") + 
   geom_abline(intercept = AWD, slope = SLOPE, color = "purple", lty = "dashed")
+
