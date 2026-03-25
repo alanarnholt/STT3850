d075f0279028f5cf7a3dc64f712fc2580b732bbb
Alan T. Arnholt <arnholtat@appstate.edu>
2026-03-25 10:22:18 -0400

    tweakers

--- Rscripts/bs_inclass.R
+++ Rscripts/bs_inclass.R
@@ -1,8 +1,8 @@
 library(resampledata)
 Bangladesh
 B <- 10000
-Bang <- Bangladesh
-cobalt <- Bang$Cobalt[-252]
+bang <- Bangladesh
+cobalt <- bang$Cobalt[-252]
 summary(cobalt)
 bs_sd <- numeric(B)
 for(i in 1:B){
@@ -15,9 +15,110 @@ quantile(bs_sd, probs = c(.05, 0.95))
 
 library(infer)
 library(tidyverse)
-Bang |> 
+bang |> 
   specify(response = Cobalt) |> 
   generate(reps = B, type = "bootstrap") |> 
   calculate(stat = "sd") -> bs_dist
 visualize(bs_dist)
 get_confidence_interval(bs_dist, level = .90)
+
+library(tidyverse)
+library(moderndive)
+library(infer)
+
+?TV
+head(TV)
+TV |> 
+  group_by(Cable) |> 
+  summarize(M = mean(Times), n = n()) -> ST
+ST
+diff_actual <- ST$M[1] - ST$M[2]
+diff_actual
+TV |> 
+  filter(Cable == "Basic") |> 
+  select(Times) |> 
+  pull() -> basic_times
+basic_times
+TV |> 
+  filter(Cable == "Extended") |> 
+  select(Times) |> 
+  pull() -> extended_times
+extended_times
+B <- 10^5
+diff_means <- numeric(B)
+for(i in 1:B){
+  basic_bss <- sample(basic_times, size = 10, replace = TRUE)
+  extended_bss <- sample(extended_times, size = 10, replace = TRUE)
+  diff_means[i] <- mean(basic_bss) - mean(extended_bss)
+}
+hist(diff_means)
+# 90% Bootstap Percentile CI
+quantile(diff_means, probs = c(0.05, 0.95)) -> BPCI
+BPCI
+
+######### Using the infer pipeline
+TV |> 
+  specify(Times ~ Cable) |> 
+  generate(reps = 10^4, type = "bootstrap") |> 
+  calculate(stat = "diff in means", order = c("Basic", "Extended")) -> bs_dist
+visualize(bs_dist) + 
+  theme_bw()
+get_confidence_interval(bs_dist, level = .90, type = "percentile")
+get_confidence_interval(bs_dist, type = "se", point_estimate = diff_actual, level = 0.90)
+diff_actual + c(-1,1)*qnorm(.95)*sd(bs_dist$stat)
+
+
+Sat2008 |> 
+  specify(response = Reading) |> 
+  generate(reps = 10^4, type = "bootstrap") |> 
+  calculate(stat = "mean") -> bs_read_dist
+bs_read_dist
+get_confidence_interval(bs_read_dist, level = 0.95)
+
+math <- Sat2008$Math
+math
+B <- 10^5
+bs_mean <- numeric(B)
+for(i in 1:B){
+  bss <- sample(math, size = sum(!is.na(math)), replace = TRUE)
+  bs_mean[i] <- mean(bss)
+}
+hist(bs_mean)
+quantile(bs_mean, probs = c(0.05, 0.95))
+
+boot_bias <- mean(bs_mean) - mean(math)
+boot_bias
+
+
+BODYFAT |> 
+  specify(fat ~ sex) |> 
+  generate(reps = 10^4, type = "bootstrap") |> 
+  calculate(stat = "diff in means", order = c("F", "M")) -> bs_dist
+get_confidence_interval(bs_dist, level = 0.95)
+
+
+BODYFAT$fat[BODYFAT$sex=="M"] -> mf
+BODYFAT$fat[BODYFAT$sex=="F"] -> ff
+
+B <- 10^4
+dm <- numeric(B)
+for(i in 1:B){
+  bssf <- sample(ff, size = 14, replace = TRUE)
+  bssm <- sample(mf, size = 4, replace = TRUE)
+  dm[i] <- mean(bssf) - mean(bssm)
+} 
+hist(dm)
+quantile(dm, probs = c(.025, 0.975))
+
+
+##############################################################
+
+manatees |> 
+  mutate(rat = killed/boats) -> nm
+nm
+hist(nm$rat)
+
+ggplot(data = manatees, aes(x = boats, y = killed)) + 
+  geom_point() + 
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE)
