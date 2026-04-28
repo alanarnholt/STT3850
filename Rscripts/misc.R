40d03e8d5c71dbd4529c52f4b855552b891dc10a
Alan T. Arnholt <arnholtat@appstate.edu>
2026-04-28 11:40:39 -0400

    tweaks

--- Rscripts/misc.R
+++ Rscripts/misc.R
@@ -1,4 +1,18 @@
-library(tidyverse)
 library(PASWR2)
-ggplot(data = GRADES, aes(x = sat, y = gpa)) +
-  geom_point()
+mini <- VIT2005 |> filter(age <= 10)
+mini <- mini |> 
+  mutate(toilets = as.factor(toilets))
+
+ggplot(data = mini, aes(x = toilets, y = totalprice)) +
+  geom_boxplot()
+
+library(infer)
+set.seed(123)
+
+
+\mini |> 
+  specify(totalprice ~ toilets) |> 
+  hypothesize(null = "independence") |> 
+  generate(reps = 10^4, type = "permute") |> 
+  calculate(stat = "diff in medians") -> null 
+visualize(null)
